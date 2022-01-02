a37ad3db85df0b498eb4d295f96bbb5da4070d0d
jajaz1 <94967729+jajaz1@users.noreply.github.com>
2022-01-02 18:44:48 +0300

    Add files via upload

---
+++ dllmain.cpp
@@ -0,0 +1,127 @@
+﻿#include "stdafx.h"
+#include <iostream>
+#include "mem.h"
+#include <WinSock2.h>
+#include "proc.h"
+
+
+void hexDump(void* addr, int len)
+{
+	int i;
+	unsigned char buff[500];
+	unsigned char* pc = (unsigned char*)addr;
+	// Process every byte in the data.
+	for (i = 0; i < len; i++) {
+		// Multiple of 16 means new line (with line offset).
+
+		if ((i % 16) == 0) {
+			// Just don't print ASCII for the zeroth line.
+			if (i != 0)
+				printf("  %s\n", buff);
+
+			// Output the offset.
+			std::cout << (int*)addr + 16 << " : ";
+		}
+
+		// Now the hex code for the specific character.
+		printf(" %02x", pc[i]);
+
+		// And store a printable ASCII character for later.
+		if ((pc[i] < 0x20) || (pc[i] > 0x7e)) {
+			buff[i % 16] = '.';
+		}
+		else {
+			buff[i % 16] = pc[i];
+		}
+
+		buff[(i % 16) + 1] = '\0';
+	}
+
+	// Pad out last line if not exactly 16 characters.
+	while ((i % 16) != 0) {
+		printf("   ");
+		i++;
+	}
+
+	// And print the final ASCII bit.
+	printf("  %s\n", buff);
+}
+
+typedef BOOL(__thiscall* _myFunc2)(int This, int* a2, int a3);
+_myFunc2 myFunc2;
+
+BOOL __fastcall Hook1(int This, int EDX, int* a2, int a3)
+{
+	char i;
+	int e;
+	int c;
+	unsigned char* pc = (unsigned char*)a2;
+
+	if ((int)a2 > 0x18 )
+	{
+		if (GetAsyncKeyState(VK_RETURN))
+		{
+			for (i = 0; i < 50; i++)
+			{
+				printf("%x :", pc + (16 * i));
+				for (e = 0; e < 16; ++e)
+				{
+					printf("%02x ", pc[e + (16 * i)]);
+				}
+				printf("\t");
+				for (c = 0; c < 16; ++c)
+				{
+					printf("%c", pc[c + (16 * i)]);
+				}
+				printf("\n");
+			}
+			printf("\n===================================================================\n");
+		}
+	}
+
+	return myFunc2(This, a2, a3);
+}
+
+
+DWORD WINAPI HackThread(HMODULE hModule)
+{
+	//Create Console
+	AllocConsole();
+	FILE* f;
+	freopen_s(&f, "CONOUT$", "w", stdout);
+
+	uintptr_t xaddr = GetModuleBaseAddress(0, L"engine.dll");
+	myFunc2 = (_myFunc2)(xaddr + 0x225790);
+	myFunc2 = (_myFunc2)mem::TrampHook32((BYTE*)myFunc2, (BYTE*)Hook1, 5);
+
+	//x0r = (_x0r)(xaddr + 0x1016D170); 
+	//x0r = (_x0r)mem::TrampHook32((BYTE*)x0r, (BYTE*)myx0, 5);
+
+	//osend = (tsend)GetProcAddress(GetModuleHandle(L"ws2_32.dll"), "sendto");
+	//osend = (tsend)mem::TrampHook32((BYTE*)osend, (BYTE*)hsend, 5);
+	while (true)
+	{
+
+	}
+	fclose(f);
+	FreeConsole();	
+	FreeLibraryAndExitThread(hModule, 0);
+	return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr));
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
---
+++ mem.cpp
@@ -0,0 +1,86 @@
+﻿#include "stdafx.h"
+#include "mem.h"
+#include <iostream>
+
+
+void mem::Patch(BYTE* dst, BYTE* src, unsigned int size)
+{
+	DWORD oldprotect;
+	VirtualProtect(dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);
+
+	memcpy(dst, src, size);
+	VirtualProtect(dst, size, oldprotect, &oldprotect);
+}
+
+void mem::PatchEx(BYTE* dst, BYTE* src, unsigned int size, HANDLE hProcess)
+{
+	DWORD oldprotect;
+	VirtualProtectEx(hProcess, dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);
+	WriteProcessMemory(hProcess, dst, src, size, nullptr);
+	VirtualProtectEx(hProcess, dst, size, oldprotect, &oldprotect);
+}
+
+void mem::Nop(BYTE* dst, unsigned int size)
+{
+	DWORD oldprotect;
+	VirtualProtect(dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);
+	memset(dst, 0x90, size);
+	VirtualProtect(dst, size, oldprotect, &oldprotect);
+}
+
+void mem::NopEx(BYTE* dst, unsigned int size, HANDLE hProcess)
+{
+	BYTE* nopArray = new BYTE[size];
+	memset(nopArray, 0x90, size);
+
+	PatchEx(dst, nopArray, size, hProcess);
+	delete[] nopArray;
+}
+
+uintptr_t mem::FindDMAAddy(uintptr_t ptr, std::vector<unsigned int> offsets)
+{
+	uintptr_t addr = ptr;
+	for (unsigned int i = 0; i < offsets.size(); ++i)
+	{
+		addr = *(uintptr_t*)addr;
+		addr += offsets[i];
+	}
+	return addr;
+}
+bool mem::Detour32(BYTE* src, BYTE* dst, const uintptr_t len)
+{
+	if (len < 5)
+	{
+		return false;
+	}
+
+	DWORD curProtection;
+	VirtualProtect(src, len, PAGE_EXECUTE_READWRITE, &curProtection);
+	uintptr_t relativeAddress = dst - src - 5;
+	*src = 0xE9;
+	*(uintptr_t*)(src + 1) = relativeAddress;
+	//*(src + 5) = 0x90;
+	VirtualProtect(src, len, curProtection, &curProtection);
+	return true;
+}
+BYTE* mem::TrampHook32(BYTE* src, BYTE* dst, uintptr_t len)
+{
+	//len = 6;
+	if (len < 5)
+	{
+		return 0;
+	}
+	BYTE* gateway = (BYTE*)VirtualAlloc(0, 10, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	memcpy_s(gateway, len, src, len);
+
+	uintptr_t gateRelativeAddr = src - gateway - 5;
+
+	*(gateway + len) = 0xE9;
+
+	//ворованные байты + адрес возврата
+	*(uintptr_t*)(uintptr_t)(gateway + len + 1) = gateRelativeAddr;
+
+	Detour32(src, dst, len);
+
+	return gateway;
+}
